90acc9aa7ac1d953698e76e8f32a9349371f0390
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-02-02 13:02:05 +0530

    first commit

---
+++ chatsapp/src/useEffect/Lectur_one.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+function Lectur_one() {
+  let [count,setCount] = useState(0);
+
+  //Write code of useEffect
+  useEffect(()=>{
+      document.title = `${count <= 0 ? "Chats": "Chats(" + count + ")"}`
+  })
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={()=>setCount(count+1)}>Message Box 📝</button>
+    </div>
+  )
+}
+
+export default Lectur_one
